80e2ec3362972b92c820e1c317ddd8affd93b650
jesse dodoo <jesse.dodoo@gmail.com>
2022-02-23 13:59:33 +0000

    second-commit

--- static/js/football.js
+++ static/js/football.js
@@ -1,7 +1,32 @@
 let heading = document.querySelector('#heading');
+let h2El = document.querySelector('#team-list')
+let bodyKeyPress = document.querySelector('body')
 
 heading.addEventListener('click', (e) => {
     let body = document.querySelector('body')
     body.classList.add('darkMode')
     console.log('click')
 })
+
+heading.addEventListener('mouseout', (e)=> {
+    let body = document.querySelector('body')
+    body.classList.remove('darkMode')
+    
+})
+
+h2El.addEventListener('mouseover', (e)=> {
+    h2El.textContent = "Hello Football Fans!"
+})
+
+bodyKeyPress.addEventListener('keypress', (e)=> {
+    let element = document.getElementById("new");
+    let tag = document.createElement("p");
+    let text = document.createTextNode(e.target.key);
+    tag.appendChild(text)
+    element.appendChild(tag)
+    
+})
+
+
+
+
